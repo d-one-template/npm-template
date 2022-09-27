b27da0caf5d11681aa67ebc947b647d156c81e98
mriliujy <mriliujy@gmail.com>
2022-09-27 18:05:09 +0800

    feat: first push

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import resolve from 'rollup-plugin-node-resolve'
+import ts from 'rollup-plugin-typescript2';
+
+const getPath = _path => path.resolve(__dirname, _path)
+const extensions = [
+  '.js',
+  '.ts',
+]
+
+// ts
+const tsPlugin = ts({
+  tsconfig: getPath('./tsconfig.json'), // 导入本地ts配置
+  extensions
+})
+
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './lib/index.js',
+    format: 'cjs'
+  },
+  plugins:[
+    resolve(extensions),
+    tsPlugin,
+  ]
+};
